b52c03bdfe7a3ea4010e71bf029584441db4aee0
Zoey Placek <zbplacek@gmail.com>
2026-09-01 12:22:16 -0400

    Enhance program to solve linear equation
    Added user prompts and calculations for solving Ax + B = 0.

--- CGT-215-LAB-02/CGT-215-Lab-02.cpp
+++ CGT-215-LAB-02/CGT-215-Lab-02.cpp
@@ -2,12 +2,24 @@
 using namespace std;
 int main()
 {
-	float A;
-	float B;
-	float X;
-	cout << "Please enter a value for A: ";
-	cin >> A;
-	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+	float A; //first variable user inputs
+	float B; //second variable user inputs
+	float X; //calculated variable from first and second user inputed variables
+
+	cout << "Hello, my name is Zoey and I will solve this equation for you:" << endl; //greeting user
+
+	cout << "Ax + B = 0" << endl; //equation that will be solved
+
+	cout << "Please enter a value for A: "; 
+	cin >> A; //user input for A
+
+	cout << "Please enter a value for B: ";
+	cin >> B; //user input for B
+
+	X = -B / A; //originally from Ax + B = 0 equation, rearranged to solve for X
+
+	cout << "Solving " << A << "x + " << B << " = 0 " << endl; //showing user its being solved
+	cout << "x = " << X << endl; //showing the answer to user
+
+	return 0;
 }
