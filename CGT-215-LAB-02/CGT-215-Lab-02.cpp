7fb11b268500a45de562127989b11f990d43b927
zplacek <zplacek@X-KNOY416PC24>
2026-09-01 11:57:17 -0400

    Add project files.

---
+++ CGT-215-LAB-02/CGT-215-Lab-02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "A=" << A << endl;
+	B = (A * 4) + 1;
+	cout << "And B=" << B << endl;
+}
